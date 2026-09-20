6b4cdf5d47fae539476b991544fd5105e586284d
ingmaricelabarcenes <mariamontenegro4444@gmail.com>
2026-09-19 23:56:09 -0500

    Add files via upload

---
+++ Calcular el área de un rectángulo.py
@@ -0,0 +1,7 @@
+def calcular_area_rectangulo(largo, ancho):
+    area = largo * ancho
+    return area
+largo = 8
+ancho = 5
+resultado = calcular_area_rectangulo(largo, ancho)
+print("El area del rectangulo es:", resultado)
